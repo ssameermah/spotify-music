5300e937305a4dee51e3fd89c97ab7dfed40d5e1
Sameer Mahajan <sam241099@gmail.com>
2020-08-12 21:08:54 +0530

    api connect

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+import { useStateValue  } from "./StateProvider";
+
+const spotify = new SpotifyWebApi();
+
+function App() {
+  const [{ user,token }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token){
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
+   
+      
+      
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user =>{
+        dispatch({
+          type: "SET_USER",
+          user : user,
+        });
+      });
+    }; 
+  },[]);
+
+ 
+
+  return <div className="app">{token ?
+    <Player spotify={spotify}/> :
+  <Login/>}</div>;
+  
+}
+
+export default App;
---
+++ src/Player.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Player.css";
+
+
+function Player({spotify}) {
+    return (
+        <div className="player">
+            <div className="player__body">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Player
